2d888f207e197fbf6d692e332b8300e334b161b7
Ebrahim aharpour <ebrahim@openweb.nl>
2017-03-02 15:24:30 +0100

    compression was added.

--- restore-backup.sh
+++ restore-backup.sh
@@ -3,9 +3,9 @@ DOCKER_VOLUMES_DIR=/var/lib/docker/volumes
 echo "Deleting $DOCKER_VOLUMES_DIR/$1_mysql_data/_data"
 rm -rf $DOCKER_VOLUMES_DIR/$1_mysql_data/_data
 echo "Unpacking mysql_backup.tar.gz into $DOCKER_VOLUMES_DIR/$1_mysql_data"
-tar -xvf mysql_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_mysql_data
+tar -xzvf mysql_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_mysql_data
 
 echo "Deleting $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data"
 rm -rf $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
 echo "Unpacking lucene_indexes_backup.tar.gz into $DOCKER_VOLUMES_DIR/$1_hippo_repository"
-tar -xvf lucene_indexes_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_hippo_repository
+tar -xzvf lucene_indexes_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_hippo_repository
