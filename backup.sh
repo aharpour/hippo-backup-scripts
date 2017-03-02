006ad7ed59f1aa00b6a63e46deca39c83d85852b
root <root@sys5>
2017-03-02 11:10:42 +0100

    The scripts have been debugged

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOCKER_VOLUMES_DIR=/var/lib/docker/volumes
 echo "Making a backup of $DOCKER_VOLUMES_DIR/$1_mysql_data"
-tar -cvf mysql_backup.tar.gz $DOCKER_VOLUMES_DIR/$1_mysql_data
+tar -C $DOCKER_VOLUMES_DIR/$1_mysql_data  -cvf mysql_backup.tar.gz _data
 
 echo "Making a backup of $DOCKER_VOLUMES_DIR/$1_hippo_repository"
-tar -cvf lucene_indexes_backup.tar.gz $DOCKER_VOLUMES_DIR/$1_hippo_repository
+tar -C $DOCKER_VOLUMES_DIR/$1_hippo_repository -cvf lucene_indexes_backup.tar.gz _data
