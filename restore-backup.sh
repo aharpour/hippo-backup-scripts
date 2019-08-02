1d20d7b83cd02dc392a49d08c0da0b153f99f9ab
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2019-08-02 12:54:42 +0200

    Updating resore script so that it adds the volume folders if they don't exists already.

--- restore-backup.sh
+++ restore-backup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 DOCKER_VOLUMES_DIR=/var/lib/docker/volumes
+[ ! -d "$DOCKER_VOLUMES_DIR/$1_mysql_data" ] && \
+echo "Creating Directory DOCKER_VOLUMES_DIR/$1_mysql_data/_data" && \
+mkdir -p $DOCKER_VOLUMES_DIR/$1_mysql_data/_data
+
 echo "Deleting $DOCKER_VOLUMES_DIR/$1_mysql_data/_data"
 rm -rf $DOCKER_VOLUMES_DIR/$1_mysql_data/_data
 echo "Unpacking mysql_backup.tar.gz into $DOCKER_VOLUMES_DIR/$1_mysql_data"
 tar -xzvf mysql_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_mysql_data
+
+[ ! -d "$DOCKER_VOLUMES_DIR/$1_hippo_repository" ] && \
+echo "Creating Directory DOCKER_VOLUMES_DIR/$1_hippo_repository/_data" && \
+echmkdir -p $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
 
 echo "Deleting $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data"
 rm -rf $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
