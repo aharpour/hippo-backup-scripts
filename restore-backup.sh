1c3aaf70602b0d850a268506fa947575e038af6b
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2019-09-05 22:06:56 +0200

    Fixing a bug in creating folder.

--- restore-backup.sh
+++ restore-backup.sh
@@ -11,7 +11,7 @@ tar -xzvf mysql_backup.tar.gz -C $DOCKER_VOLUMES_DIR/$1_mysql_data
 
 [ ! -d "$DOCKER_VOLUMES_DIR/$1_hippo_repository" ] && \
 echo "Creating Directory DOCKER_VOLUMES_DIR/$1_hippo_repository/_data" && \
-echmkdir -p $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
+mkdir -p $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
 
 echo "Deleting $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data"
 rm -rf $DOCKER_VOLUMES_DIR/$1_hippo_repository/_data
